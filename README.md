e166ba6824df5deb2efe3da59fa9c3729d77186d
Remco Tolsma <info@remcotolsma.nl>
2015-11-11 14:53:12 +0100

    Simplified integration class.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # WordPress Pay Gateway: iDEAL Simulator - iDEAL Professional / Advanced / Zelfbouw - v3
 
 **iDEAL Simulator - iDEAL Professional / Advanced / Zelfbouw - v3 driver for the WordPress payment processing library.**
+
+## Test Environment
+
+**Payment Server URL:** https://www.ideal-checkout.nl/simulator/  
--- src/Integration.php
+++ src/Integration.php
@@ -1,8 +1,10 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_IDealSimulator_IDealAdvancedV3_GatewayIntegration {
+class Pronamic_WP_Pay_Gateways_IDealSimulator_IDealAdvancedV3_Integration {
 	public function __construct() {
-		$this->id = 'ideal-simulator-ideal-advanced-v3';
+		$this->id       = 'ideal-simulator-ideal-advanced-v3';
+		$this->name     = 'iDEAL Simulator - iDEAL Professional / Advanced / Zelfbouw - v3';
+		$this->provider = 'ideal-simulator';
 	}
 
 	public function get_config_factory_class() {
