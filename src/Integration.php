42d9cb24b2e50a8fa865f1c8208f4abd5320899e
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 13:26:23 +0100

    Extend from iDEAL Advanced library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,21 +1,9 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_IDealSimulator_IDealAdvancedV3_Integration {
+class Pronamic_WP_Pay_Gateways_IDealSimulator_IDealAdvancedV3_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvancedV3_AbstractIntegration {
 	public function __construct() {
 		$this->id       = 'ideal-simulator-ideal-advanced-v3';
 		$this->name     = 'iDEAL Simulator - iDEAL Professional / Advanced / Zelfbouw - v3';
 		$this->provider = 'ideal-simulator';
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvancedV3_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvancedV3_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvancedV3_Gateway';
 	}
 }
