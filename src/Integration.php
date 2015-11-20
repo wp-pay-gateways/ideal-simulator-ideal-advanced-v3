617390353bbba19bca9c85382c67bd8594e3f679
Remco Tolsma <info@remcotolsma.nl>
2015-11-20 12:07:58 +0100

    Added an custom config factory class.

--- src/Integration.php
+++ src/Integration.php
@@ -1,9 +1,21 @@
 <?php
 
+/**
+ * Title: iDEAL Simulator - iDEAL Professional / Advanced / Zelfbouw - v3 integration
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2015
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
 class Pronamic_WP_Pay_Gateways_IDealSimulator_IDealAdvancedV3_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvancedV3_AbstractIntegration {
 	public function __construct() {
 		$this->id       = 'ideal-simulator-ideal-advanced-v3';
 		$this->name     = 'iDEAL Simulator - iDEAL Professional / Advanced / Zelfbouw - v3';
 		$this->provider = 'ideal-simulator';
+	}
+
+	public function get_config_factory_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealSimulator_IDealAdvancedV3_ConfigFactory';
 	}
 }
