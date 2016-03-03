9dd2ca10e19ee9a4de3e560899c1576a4a8d69ab
Reüel van der Steege <reuel@pronamic.nl>
2016-03-03 16:44:40 +0100

    Add product URL

--- src/Integration.php
+++ src/Integration.php
@@ -11,9 +11,10 @@
  */
 class Pronamic_WP_Pay_Gateways_IDealSimulator_IDealAdvancedV3_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvancedV3_AbstractIntegration {
 	public function __construct() {
-		$this->id       = 'ideal-simulator-ideal-advanced-v3';
-		$this->name     = 'iDEAL Simulator - iDEAL Professional / Advanced / Zelfbouw - v3';
-		$this->provider = 'ideal-simulator';
+		$this->id          = 'ideal-simulator-ideal-advanced-v3';
+		$this->name        = 'iDEAL Simulator - iDEAL Professional / Advanced / Zelfbouw (v3)';
+		$this->provider    = 'ideal-simulator';
+		$this->product_url = 'https://www.ideal-checkout.nl/support/ideal-simulator';
 	}
 
 	public function get_config_factory_class() {
